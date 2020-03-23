d22b6c493bfd7c3b96a44de48319b37cc733534f
Rahul Gaur <rahul.nbg@gmail.com>
2020-03-24 02:54:25 +0530

    chore: tyding things up

--- .vuesion/webpack.config.js
+++ .vuesion/webpack.config.js
@@ -7,12 +7,40 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = (config, target) => {
+  console.log(config);
+  // "resolve": {
+  //   "extensions": [".mjs", ".js", ".jsx"]
+  // }
+  let configureWebpack = {
+    resolve: {
+      // .mjs needed for https://github.com/graphql/graphql-js/issues/1272
+      extensions: ['*', '.mjs', '.js', '.vue', '.json', '.gql', '.graphql']
+    },
+    module: {
+      rules: [ // fixes https://github.com/graphql/graphql-js/issues/1272
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto'
+        }
+      ]
+    }
+  };
+  config.module.rules = config.module.rules.concat(configureWebpack.module.rules);
+  // config.resolve.extensions = configureWebpack.resolve.extensions;
   if (target === 'client' && isProd) {
+    console.log(config);
     config.performance = {
       hints: 'warning',
       maxEntrypointSize: 400000,
       maxAssetSize: 300000,
     };
+    // config.rules.push({
+    //   test: /\.mjs$/,
+    //   include: /node_modules/,
+    //   type: "javascript/auto"
+    // })
+
   }
 
   return config;
